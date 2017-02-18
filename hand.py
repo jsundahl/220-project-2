62f95270b045dfa80cd673dd7cf7ee2614cab359
jsundahl <jsundahl15@gmail.com>
2017-02-18 16:25:10 -0500

    implement score_hand in Hand

--- hand.py
+++ hand.py
@@ -19,9 +19,25 @@ class Hand:
     def has_ace(self):
         return any(card.rank == "A" for card in self._cards)
 
+    @staticmethod
+    def _rank_in(card_rank, rank_list):
+        return any(card_rank == rank for rank in rank_list)
+
     def score_hand(self, aces_high):
-        # TODO
-        return NotImplemented
+        hand_sum = 0
+        numbers = [str(n) for n in range(2, 11)]
+        face_cards = list('JQK')
+        for card in self._cards:
+            if self._rank_in(card.rank, numbers):
+                hand_sum += int(card.rank)
+            elif self._rank_in(card.rank, face_cards):
+                hand_sum += 10
+            else:
+                if aces_high:
+                    hand_sum += 11
+                else:
+                    hand_sum += 1
+        return copy.copy(hand_sum)
 
     def get_cards(self):
         return copy.copy(self._cards)
